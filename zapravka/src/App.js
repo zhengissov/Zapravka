9e5ea1a3220aeaba481ee742bacdbb61bb2d9e16
aydoszhengissov <aydoszhengissov@gmail.com>
2018-09-15 18:32:20 +0600

    initial commit

---
+++ zapravka/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { RoNavbarw, Nav, NavDropdown, MenuItem, Navbar, NavItem } from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Zapravka
+      </div>
+    );
+  }
+}
+
+export default App;
