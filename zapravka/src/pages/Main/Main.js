62c513182267bb5f82a7da8046ba0c1fd48d0385
aydoszhengissov <aydoszhengissov@gmail.com>
2018-09-22 15:34:50 +0600

    redux

--- zapravka/src/pages/Main/Main.js
+++ zapravka/src/pages/Main/Main.js
@@ -10,7 +10,7 @@ const MainPage = () => (
       <div className="container">
         <div className="titles">
           <div className="titles_first">
-            Find Your Perfect Pit Stop !!! Daryn
+            Find Your Perfect Pit Stop !!! Daryn Aydos
             </div>
           <div className="titles_second">
             Find the Best Gas Prices and everything else you need to fuel your journey
