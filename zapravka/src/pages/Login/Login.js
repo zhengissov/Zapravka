5c0c1d68ebea8e2012fab7c8b808557906a2ba82
Daryn <daryn.nasa@gmail.com>
2018-09-29 15:15:39 +0600

    .

--- zapravka/src/pages/Login/Login.js
+++ zapravka/src/pages/Login/Login.js
@@ -1,25 +1,21 @@
 import React, { Component } from "react";
-import './Login.css';
+import "./Login.css";
 
 class Login extends Component {
   render() {
     return (
       <div className="containerLogin">
         <div className="column-A">
-          <span className="titleLogin">
-          Zapravka
-          </span>
-          <h6>Sign in</h6>
-          <div className="inputs">
-          <input className="email" type="text"/>
-          <input className="password" type="text"/>
-          </div>
-          
+          <span className="titleLogin">Zapravka</span>
+          <h6>SIGN IN</h6>
+          <form className="inputs">
+            <input className="email" type="text" placeholder="email" />
+            <input className="password" type="text" />
+          </form>
+
           <h6>Forgot Password?</h6>
-          <button>Sign In</button>
-          <h6>Don’t have an account? 
-          SIGNUP to ZAPRAVKA account.
-          </h6>
+          <button className="signIn">SIGN IN</button>
+          <h6>Don’t have an account? SIGNUP to ZAPRAVKA account.</h6>
         </div>
         <div className="column-B">
           <h1>WELCOME BACK</h1>
